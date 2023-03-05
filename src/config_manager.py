a590cd0150ebff279ba01aceb172a307b87b5876
José Mascarelli <josemascarelli99@gmail.com>
2023-03-05 20:32:52 -0300

    adição de comentários e melhorado classe ActiveDirectory

--- src/config_manager.py
+++ src/config_manager.py
@@ -1,16 +1,15 @@
 import configparser
 import os
 
-'''
- Esse arquivo descreve a classe que cria o arquivo de configuração 
- e acessa seu conteúdo através de seus métodos
-'''
+
+# Define a ConfigParser class to handle configuration file settings
 class ConfigParser():
     def __init__(self):
+        # Define path to configuration file
         self.file = 'config.ini'
-        # Verifica se o arquivo de configuração já existe
+        # Check if configuration file exists, if not create it.
         if not os.path.exists(self.file):
-            # Cria um novo arquivo de configuração:
+            # Create a new configuration file:
             config = configparser.ConfigParser()
             config.add_section('LDAP')
             config.set('LDAP', 'server', 'ldap://domain.com')
@@ -19,24 +18,28 @@ class ConfigParser():
             config.set('LDAP', 'searchbase', 'dc=domain,dc=com')
             config.set('LDAP', 'searchfilter', '(&(objectCategory=person)(objectClass=user)(sAMAccountName={user}))')
             config.set('LDAP', 'attributes', 'displayName, mail, telephoneNumber, streetAddress, postalCode')
-
+            
+            # Add Signature section and settings
             config.add_section('Signature')
             config.set('Signature', 'templatedir', '.')
             config.set('Signature', 'templatefile', 'signature_template.html')
             config.set('Signature', 'signaturefile', 'assinatura.htm')
                 
+            # Add URL section and settings
             config.add_section('URL')
             config.set('URL', 'image01', 'https://example.com/image01.jpg')
             config.set('URL', 'image02', 'https://example.com/image02.jpg')
             config.set('URL', 'image03', 'https://example.com/image03.jpg')
-            # Cria um novo arquivo de configuração
+            # Write configuration file
             with open(self.file, 'w') as file:
                 config.write(file)
                 print(f'Created {self.file} successfully.')
-
+        
+        # Initialize ConfigParser object and read configuration from file
         self.config = configparser.ConfigParser()
         self.config.read(self.file)
-    
+        
+    # Function to get configuration setting value
     def get(self, section, option):
         value = self.config.get(section, option)
         return value
--- src/ldap_manager.py
+++ src/ldap_manager.py
@@ -1,21 +1,45 @@
+from typing import List
 from ldap3 import Server, Connection
 
 
-class ActiveDirectory():
-    def __init__(self, server, username, password, search_base):
+
+class ActiveDirectory:
+    
+    def __init__(self, server: str, username: str, password: str, search_base: str) -> None:
         self.server = server
         self.username = username
         self.password = password
         self.search_base = search_base
-
-    def get_user_attr(self, username, search_filter=None, attributes=None):
-        with Connection(Server(self.server), self.username, self.password, auto_bind=True) as conn:
+    
+    def get_user_attr(self, username: str, search_filter: str = None, attributes: List[str] = None) -> dict:
+        
+        # Create a connection object and perform a search for the given username
+        try:
+            conn = Connection(Server(self.server), user=self.username, password=self.password)
+            conn.bind()
+            
             conn.search(
                 search_base=self.search_base,
                 search_filter=search_filter.format(user=username),
                 attributes=attributes
             )
-            if conn.entries:
-                entry = conn.entries[0]
-                user_attr = {attribute: entry[attribute].value for attribute in attributes if attribute in entry}
-                return user_attr
+           
+        except Exception as e:
+            raise ValueError(f"User {self.username} was not able to bind to LDAP server. Error: {str(e)}")
+
+        # If we didn't find any entries based on the given search at Active Directory,
+        if len(conn.entries) < 1:
+            return {}
+
+        # If there are multiple entries for the username, it may cause issues, thus we handle such case by returning an empty dictionary
+        elif len(conn.entries) > 1:
+            raise ValueError(f"Multiple entries found for '{username}' in Active Directory.")
+            
+        entry = conn.entries[0]
+        
+        # Get all of the attributes that are present on this user's entry 
+        # Only retrieves specified attributes while initializing the class object. 
+        user_attr = {attribute: entry[attribute].value for attribute in attributes if attribute in entry}
+        
+        # Return a dictionary containing relevant user attributes.
+        return user_attr
